52d9457f0b48da2331a45437c8fc7ddf14ebd95b
anurag kumar <anurag3rdsep@gmail.com>
2013-11-30 10:53:30 +0530

    GET now working

--- mod.py
+++ mod.py
@@ -1,25 +1,14 @@
 from collections import defaultdict
 import pymongo
 import re
-import json
 
 connection = pymongo.Connection()
 db = connection.mydatabase
 activity = db.activity
 
-
-def con(dicn):
-    """Recursively converts dictionary keys to strings."""
-    if not isinstance(dicn, dict):
-        return dicn
-    return dict((str(k), con(v)) 
-        for k, v in dicn.items())
-
-
 def deconv(di):
 	
 	qw = re.search(r'\b[A-Z]{3}\b',di)
-
 	q= create(qw.group())
 	return q
 
@@ -28,9 +17,8 @@ def tree():
 
 def create(er):
 	a= tree()
-	b = a[er[0]][er[1]][er[2]]
+	a[er[0]][er[1]][er[2]]
 	gh = nes(a)
-
 	return gh
 
 
